2e817995361dc8fabe6ca27dae11d965ebba1fb1
dkothari104 <dkothari@digite.com>
2016-05-25 15:37:14 +0530

    IssueId::DEF13 Reviewer::admin Status::Done Comments::The defect has finally been resolved

--- InfosysDemo.java
+++ InfosysDemo.java
@@ -3,7 +3,7 @@ public class InfosysDemo
 {
 	public static void main(String args [])
 	{
-		System.out.println("This is a commit against Defect in SwiftKanban with ID DEF13");
+		System.out.println("This is the  commit against Defect in SwiftKanban with ID DEF13");nd
 		//test commit
 	}
 
