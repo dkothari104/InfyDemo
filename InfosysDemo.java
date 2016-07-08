f972fd703bd34dd15e58e1368ae8eaf7818f3c32
dkothari104 <dkothari@digite.com>
2016-07-08 11:01:49 +0530

    IssueId::UST1 Reviewer::admin Status::Done Comments::test

--- InfosysDemo.java
+++ InfosysDemo.java
@@ -3,7 +3,7 @@ public class InfosysDemo
 {
 	public static void main(String args [])
 	{
-		System.out.println("This is the  commit against Defect in SwiftKanban with ID DEF13");nd
+		System.out.println("This is the  commit against Defect in SwiftKanban with ID DEF13");
 		//test commit
 	}
 
